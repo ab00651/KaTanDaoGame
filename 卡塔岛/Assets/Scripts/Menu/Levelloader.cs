ca77df4a5c1b386d9fc09bcf94b862afff1f767b
SALTEDfish <156570474+ab00651@users.noreply.github.com>
2026-06-19 11:19:42 +0800

    项目基础文件

---
+++ 卡塔岛/Assets/Scripts/Menu/Levelloader.cs
@@ -0,0 +1,33 @@
+  using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Levelloader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetMouseButtonDown(0)) {
+            LoadNextLevel();
+        }*/
+    }
+
+    private int targetLevelIndex;
+
+    public void LoadNextLevel()
+    {
+        targetLevelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        transition.SetTrigger("End");
+        Invoke("DoLoadLevel", transitionTime);
+    }
+
+    void DoLoadLevel()
+    {
+        SceneManager.LoadScene(targetLevelIndex);
+    }
+}
+
